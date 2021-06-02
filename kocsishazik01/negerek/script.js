afa37cc81de42dace911415ef99f3ab2eadf4e3f
serbiancumrad <sleepydog898@gmail.com>
2021-06-02 17:21:09 +0200

    confirm

--- kocsishazik01/negerek/script.js
+++ kocsishazik01/negerek/script.js
@@ -1,20 +1,20 @@
 function findH(){
-  var sideA = document.getElementById("a").value;
-  var sideB = document.getElementById("b").value;
+  var A = document.getElementById("a").value;
+  var B = document.getElementById("b").value;
  
 
-  if(sideA<1 || sideB<1){
+  if(A<1 || B<1){
      alert("cigane irjal be egy rendes szamot!");
      }else{
   //a negyzeten es b negyzeten
-  document.getElementById("aSqu").innerHTML = sideA * sideA;
-  document.getElementById("bSqu").innerHTML = sideB * sideB;
+  document.getElementById("aSqu").innerHTML = A * A;
+  document.getElementById("bSqu").innerHTML = B * B;
   
   //a es b 
-  document.getElementById("aAndB").innerHTML = (sideA * sideA) + (sideB * sideB);
+  document.getElementById("aAndB").innerHTML = (A * A) + (B * B);
   
   //átfogó
-   document.getElementById("sqrtC").innerHTML = Math.round((Math.sqrt((sideA * sideA) + (sideB * sideB)) * 100)) / 100;
+   document.getElementById("sqrtC").innerHTML = Math.round((Math.sqrt((A * A) + (B * B)) * 100)) / 100;
   
  }
 }
