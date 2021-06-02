76b4149583e8e45ab19c75fd2936b6815f8ed4a3
serbiancumrad <sleepydog898@gmail.com>
2021-06-02 12:36:21 +0200

    confirm

---
+++ kocsishazik01/negerek/script.js
@@ -0,0 +1,20 @@
+function findH(){
+  var sideA = document.getElementById("a").value;
+  var sideB = document.getElementById("b").value;
+ 
+
+  if(sideA<1 || sideB<1){
+     alert("cigane irjal be egy rendes szamot!");
+     }else{
+  //a negyzeten es b negyzeten
+  document.getElementById("aSqu").innerHTML = sideA * sideA;
+  document.getElementById("bSqu").innerHTML = sideB * sideB;
+  
+  //elso szam
+  document.getElementById("aAndB").innerHTML = (sideA * sideA) + (sideB * sideB);
+  
+  //átfogó
+   document.getElementById("sqrtC").innerHTML = Math.round((Math.sqrt((sideA * sideA) + (sideB * sideB)) * 100)) / 100;
+  
+ }
+}
